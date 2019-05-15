310e9b457e0fd270371f5b9746df04df002f2357
EmpressOfWinter <50548937+EmpressOfWinter@users.noreply.github.com>
2019-05-15 18:08:06 +0200

    Update S_CAMERA.cs

--- Assets/SCRIPTS/S_CAMERA.cs
+++ Assets/SCRIPTS/S_CAMERA.cs
@@ -18,8 +18,7 @@ public class S_CAMERA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Switch to 640 x 480 full-screen
-        Screen.SetResolution(640, 480, true);
+       
     }
 
     // Update is called once per frame
