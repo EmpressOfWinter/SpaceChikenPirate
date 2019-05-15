34a9265d04d5eeda6dc71ad95901b034b49b7d35
AMP\phouot <phouot@amplitude-studios.com>
2019-05-15 09:53:46 +0200

    First commit

---
+++ Assets/SCRIPTS/S_ENEMY.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_ENEMY : MonoBehaviour
+{
+    public int damage = 1;
+
+    public float speed;
+    public GameObject effect;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+
+        if (other.CompareTag("Player"))
+        {
+            //player takes damage
+
+            other.GetComponent<Player>().health--;
+            
+            //Instantiate(effect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+
+        if (other.CompareTag("Borders"))
+        {
+        Destroy(gameObject);
+        }
+           
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+
+    }
+}
---
+++ Assets/SCRIPTS/SpawnPOint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPOint : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    private void Start()
+    {
+        Instantiate(obstacle, transform.position, Quaternion.identity);
+    }
+}
